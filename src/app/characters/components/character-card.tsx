8c4a9a0aa9513c4cfc133e9b10a71bee78a547f2
Mike Theodorou <miketheocodes@gmail.com>
2025-01-03 13:24:23 -0700

    feat: improves character card UI

--- src/app/characters/components/character-card.tsx
+++ src/app/characters/components/character-card.tsx
@@ -1,4 +1,5 @@
 import { CharactersQuery } from '@/services/rickandmorty/generated/graphql';
+import { Dna, Skull } from 'lucide-react';
 import Image from 'next/image';
 
 export interface CharacterCardProps {
@@ -8,17 +9,35 @@ export interface CharacterCardProps {
 }
 export function CharacterCard({ character }: CharacterCardProps) {
   return (
-    <div className='rounded-lg shadow-lg p-4 border border-white'>
-      <Image
-        src={character.image ?? ''}
-        alt={character.name ?? 'Character'}
-        width={160}
-        height={160}
-        className='rounded-full border-2 border-white mx-auto mb-4'
-      />
-      <h2>{character.name}</h2>
-      <p>{character.species}</p>
-      <p>{character.status}</p>
+    <div className='rounded-lg shadow-xl p-4 border border-violet-400'>
+      <div className='mb-4'>
+        <Image
+          src={character.image ?? ''}
+          alt={character.name ?? 'Character'}
+          width={160}
+          height={160}
+          className='rounded-full border-2 border-violet-400 mx-auto mb-2'
+        />
+        <h2 className='font-bold text-center text-lg'>{character.name}</h2>
+      </div>
+
+      <dl className='text-sm flex gap-4 flex-wrap justify-center'>
+        <div className='text-center'>
+          <div className='flex items-center gap-1 mb-1 font-bold'>
+            <Dna className='size-4' />
+            <dt>Species</dt>
+          </div>
+          <dd>{character.species}</dd>
+        </div>
+
+        <div className='text-center'>
+          <div className='flex items-center gap-1 mb-1 font-bold'>
+            <Skull className='size-4' />
+            <dt className='font-bold '>Status</dt>
+          </div>
+          <dd>{character.status}</dd>
+        </div>
+      </dl>
     </div>
   );
 }
